37676423da05c427e5ddd051eff51c255d387301
monosniper <monosniper@gmail.com>
2024-02-04 18:16:40 +0500

    dasasd

--- src/components/categories/shop/ShopCategoryCreate.js
+++ src/components/categories/shop/ShopCategoryCreate.js
@@ -1,25 +1,14 @@
-import React, {useEffect, useState} from "react";
-import {useGetList} from "ra-core";
-import {AutocompleteInput, Create, required, SimpleForm, TextInput} from "react-admin";
+import React from "react";
+import {AutocompleteInput, Create, required, SimpleForm, ReferenceInput, TextInput} from "react-admin";
 
 export const ShopCategoryCreate = (props) => {
-    const [categories, setCategories] = useState([]);
-    const { data, loading, error } = useGetList('categories/shop', {filter: false});
-
-    useEffect(() => {
-        !loading && setCategories(Object.entries(data).map(item => {
-            return {
-                id: item[1].id,
-                name: item[1].name,
-            }
-        }))
-    }, [data]);
-
     return (
         <Create title="Создать категорию" {...props}>
             <SimpleForm>
                 <TextInput validate={required()} label='Название' source="name" />
-                <AutocompleteInput label='Родитель' choices={categories} source="parent_id" />
+                <ReferenceInput label="Родитель" source="parent_id" reference="categories/shop">
+                    <AutocompleteInput optionText="name" optionValue="id" />
+                </ReferenceInput>
             </SimpleForm>
         </Create>
     );
